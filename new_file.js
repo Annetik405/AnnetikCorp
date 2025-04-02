26a8d8394d289c72b88b94230e692cc020a38eb1
anecka <akononenko405@gmail.com>
2025-04-02 22:14:45 +0300

    change .gitignore

--- new_file.js
+++ new_file.js
@@ -1,9 +1,11 @@
-const text = "Wonderful, Joyful Happiness, Time, Task, Apple";
+const text = "Wonderful, Joyful, Happiness, Time, Task, Apple";
 
-const pattern = /?[]/g;
+const pattern = /[^a,A,p]{6,}/g;
 
-//const result = text.match(pattern);
+const result = text.match(pattern);
 console.log(result);
 
 //Напишите регулярный текст, который знает количество шести или
 // больше символов, не содержащих буквы «А» (очень или мало)
+//Повинен знаходити: Wonderful, Joyful
+// Не повинен знаходити: Happiness, Time, Task, Apple
