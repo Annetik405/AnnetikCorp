c50c10fe91b9c6c9cf872303bbcfbb590ab823da
anecka <akononenko405@gmail.com>
2025-04-02 19:11:14 +0300

    change .gitignore

---
+++ new_file.js
@@ -0,0 +1,9 @@
+const text = "Wonderful, Joyful Happiness, Time, Task, Apple";
+
+const pattern = /?[]/g;
+
+//const result = text.match(pattern);
+console.log(result);
+
+//Напишите регулярный текст, который знает количество шести или
+// больше символов, не содержащих буквы «А» (очень или мало)
